fc52d1affd4df21f90ca5dbc7736f84dc706bb07
Adrian Berges Enfedaque <aberges@gradiant.org>
2023-07-16 22:52:44 +0200

    made a module

--- src/julia_sets.jl
+++ src/julia_sets.jl
@@ -1,19 +1,11 @@
 using Images
 using ColorSchemes
 
+module JuliaSets
+
+export f_c, escape_time
+
 f_c(c::Complex) = z::Complex -> z^2 + c
-
-n = 1600 * 2 # 3200
-m = 925 * 2 # 1850
-c = -0.77146 - 0.10119im
-N = 3000
-width = [-1.65 1.65]
-height = width * m / n
-
-complex_grid = [complex(a, b)
-                for a in range(width[1], length=n, stop=width[2]),
-                b in range(height[1], length=m, stop=height[2])];
-
 
 function escape_time(f::Function, z::Complex, maxiter::Int64=10^3)::Int64
     for n = 1:maxiter
@@ -25,10 +17,35 @@ function escape_time(f::Function, z::Complex, maxiter::Int64=10^3)::Int64
     return maxiter
 end
 
-# compute
-@time J_f = map(z -> escape_time(f_c(c), z, N), complex_grid)'
+end
 
-cs = ColorSchemes.diverging_rainbow_bgymr_45_85_c67_n256
-scale(X) = X / maximum(X)
+parameters = (
+    n=1600 * 2, # 3200
+    m=925 * 2, # 1850
+    c=-0.77146 - 0.10119im,
+    N=3000,
+    width=[-1.65 1.65],
+)
 
-save("julia_set.png", J_f .|> log1p |> scale .|> x -> ifelse(x == 1, RGB(0, 0, 0), get(cs, x)))
+using .JuliaSets
+
+function main(parameters::NamedTuple)::Nothing
+    n, m, c, N, width = parameters
+    height = width * m / n
+
+    complex_grid = [complex(a, b)
+                    for a in range(width[1], length=n, stop=width[2]),
+                    b in range(height[1], length=m, stop=height[2])]
+
+    # compute
+    f = f_c(c)
+    @time J_f = (complex_grid .|> z -> escape_time(f, z, N))'
+    
+    cs = ColorSchemes.diverging_rainbow_bgymr_45_85_c67_n256
+    scale(X) = X / maximum(X)
+ 
+    save("julia_set.png", J_f .|> log1p |> scale .|> x -> ifelse(x == 1, RGB(0, 0, 0), get(cs, x)))
+    nothing
+end
+
+main(parameters)
