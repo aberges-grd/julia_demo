caca1cba90f2c5254dd1031725d60d7671791d4f
Adrian Berges Enfedaque <aberges@gradiant.org>
2023-07-17 19:16:13 +0200

    small changes

--- src/colorize_sets.jl
+++ src/colorize_sets.jl
@@ -7,10 +7,11 @@ using JLD2
 function render_2d()
     # cs = ColorSchemes.diverging_rainbow_bgymr_45_85_c67_n256
     # colorizer(x) = cs[x]
+    ϵ = 0.0058
     scaler(X) = (X .- minimum(X))/(maximum(X) - minimum(X))
-    colorizer(x) = ifelse(x < 9e-1, RGB(0, 0, 0), RGB(1,1,1)) # B/N
+    colorizer(x) = ifelse(x < ϵ, RGB(0, 0, 0), RGB(1,1,1)) # B/N
     J = load("julia_sets.jld", "J_f")
-    save("julia_set.png", J |> scaler .|> colorizer)
+    save("julia_set.png", J .|> colorizer)
 end
 
 render_2d()
